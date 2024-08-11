576bfdbcbdfe560dbb1809e5a66b36b48c300d84
hiberfil.sys <96713354+capacitor1@users.noreply.github.com>
2024-08-11 16:00:45 +0800

    Add files via upload

---
+++ BytesEncoder/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace BytesEncoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EncC = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Run = new System.Windows.Forms.Button();
+            this.Standard = new System.Windows.Forms.RadioButton();
+            this.Compressed = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Mode = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.HCT = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Input.Location = new System.Drawing.Point(44, 137);
+            this.Input.MaxLength = 2147438647;
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.Input.Size = new System.Drawing.Size(541, 560);
+            this.Input.TabIndex = 0;
+            // 
+            // Output
+            // 
+            this.Output.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Output.Location = new System.Drawing.Point(698, 135);
+            this.Output.MaxLength = 2147438647;
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.Output.Size = new System.Drawing.Size(539, 560);
+            this.Output.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("杨任东竹石体-Regular", 18F);
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "兽音译者 C#版";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("杨任东竹石体-Regular", 12F);
+            this.label2.Location = new System.Drawing.Point(210, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(653, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "将输入的数据按每2bit分割，使用设定的编码字符进行编码。支持压缩文本以减少编码长度。";
+            // 
+            // EncC
+            // 
+            this.EncC.Location = new System.Drawing.Point(100, 14);
+            this.EncC.MaxLength = 4;
+            this.EncC.Name = "EncC";
+            this.EncC.Size = new System.Drawing.Size(100, 25);
+            this.EncC.TabIndex = 4;
+            this.EncC.Text = "嗷呜喵~";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("杨任东竹石体-Regular", 12F);
+            this.label3.Location = new System.Drawing.Point(6, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "编码字符：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.HCT);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.Mode);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.Compressed);
+            this.groupBox1.Controls.Add(this.Standard);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.EncC);
+            this.groupBox1.Font = new System.Drawing.Font("杨任东竹石体-Regular", 12F);
+            this.groupBox1.Location = new System.Drawing.Point(45, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1192, 77);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "控制台";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("杨任东竹石体-Regular", 12F);
+            this.label4.Location = new System.Drawing.Point(206, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(240, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "（限制输入4个字符，不能重复）";
+            // 
+            // Run
+            // 
+            this.Run.Font = new System.Drawing.Font("杨任东竹石体-Regular", 12F);
+            this.Run.Location = new System.Drawing.Point(599, 170);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(88, 29);
+            this.Run.TabIndex = 7;
+            this.Run.Text = "运行";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // Standard
+            // 
+            this.Standard.AutoSize = true;
+            this.Standard.Checked = true;
+            this.Standard.Location = new System.Drawing.Point(100, 47);
+            this.Standard.Name = "Standard";
+            this.Standard.Size = new System.Drawing.Size(90, 22);
+            this.Standard.TabIndex = 7;
+            this.Standard.TabStop = true;
+            this.Standard.Text = "标准编码";
+            this.Standard.UseVisualStyleBackColor = true;
+            // 
+            // Compressed
+            // 
+            this.Compressed.AutoSize = true;
+            this.Compressed.Location = new System.Drawing.Point(209, 47);
+            this.Compressed.Name = "Compressed";
+            this.Compressed.Size = new System.Drawing.Size(90, 22);
+            this.Compressed.TabIndex = 8;
+            this.Compressed.Text = "压缩编码";
+            this.Compressed.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("杨任东竹石体-Regular", 12F);
+            this.label5.Location = new System.Drawing.Point(6, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "模式选择：";
+            // 
+            // Mode
+            // 
+            this.Mode.AutoSize = true;
+            this.Mode.Checked = true;
+            this.Mode.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.Mode.Location = new System.Drawing.Point(743, 21);
+            this.Mode.Name = "Mode";
+            this.Mode.Size = new System.Drawing.Size(219, 22);
+            this.Mode.TabIndex = 10;
+            this.Mode.Text = "打勾为编码，不打勾为解码";
+            this.Mode.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("杨任东竹石体-Regular", 12F);
+            this.label6.Location = new System.Drawing.Point(649, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "功能选择：";
+            // 
+            // HCT
+            // 
+            this.HCT.AutoSize = true;
+            this.HCT.Location = new System.Drawing.Point(743, 45);
+            this.HCT.Name = "HCT";
+            this.HCT.Size = new System.Drawing.Size(267, 22);
+            this.HCT.TabIndex = 12;
+            this.HCT.Text = "高级压缩（实验功能，谨慎勾选）";
+            this.HCT.UseVisualStyleBackColor = true;
+            this.HCT.CheckedChanged += new System.EventHandler(this.HCT_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1280, 717);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "兽音译者 C#版";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox EncC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.RadioButton Compressed;
+        private System.Windows.Forms.RadioButton Standard;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox Mode;
+        private System.Windows.Forms.CheckBox HCT;
+    }
+}
+
---
+++ BytesEncoder/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Compression;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace BytesEncoder
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // 使用System.IO.Compression进行Deflate压缩
+        public static byte[] MicrosoftCompress(byte[] data)
+        {
+            MemoryStream uncompressed = new MemoryStream(data); // 这里举例用的是内存中的数据；需要对文本进行压缩的话，使用 FileStream 即可
+            MemoryStream compressed = new MemoryStream();
+            DeflateStream deflateStream = new DeflateStream(compressed, CompressionMode.Compress); // 注意：这里第一个参数填写的是压缩后的数据应该被输出到的地方
+            uncompressed.CopyTo(deflateStream); // 用 CopyTo 将需要压缩的数据一次性输入；也可以使用Write进行部分输入
+            deflateStream.Close();  // 在Close中，会先后执行 Finish 和 Flush 操作。
+            byte[] result = compressed.ToArray();
+            return result;
+        }
+        // 使用System.IO.Compression进行Deflate解压
+        public static byte[] MicrosoftDecompress(byte[] data)
+        {
+            MemoryStream compressed = new MemoryStream(data);
+            MemoryStream decompressed = new MemoryStream();
+            DeflateStream deflateStream = new DeflateStream(compressed, CompressionMode.Decompress); // 注意： 这里第一个参数同样是填写压缩的数据，但是这次是作为输入的数据
+            deflateStream.CopyTo(decompressed);
+            byte[] result = decompressed.ToArray();
+            return result;
+        }
+        public string[] dict = { "00", "01", "10", "11" };
+        /// <summary>
+        /// 字符串转二进制
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private string StringToBinary(string str)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(str);
+            StringBuilder sb = new StringBuilder(data.Length * 8);
+            foreach (byte item in data)
+            {
+                sb.Append(Convert.ToString(item, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+        private string StringToBinarywithComp(string str)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(str);
+            byte[] datac = MicrosoftCompress(data);
+            StringBuilder sb = new StringBuilder(datac.Length * 8);
+            foreach (byte item in datac)
+            {
+                sb.Append(Convert.ToString(item, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 二进制转字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private string BinaryToString(string str)
+        {
+            System.Text.RegularExpressions.CaptureCollection cs = System.Text.RegularExpressions.Regex.Match(str, @"([01]{8})+").Groups[1].Captures;
+            byte[] data = new byte[cs.Count];
+            for (int i = 0; i < cs.Count; i++)
+            {
+                data[i] = Convert.ToByte(cs[i].Value, 2);
+            }
+            return Encoding.Unicode.GetString(data, 0, data.Length);
+        }
+        private string BinaryToStringwithComp(string str)
+        {
+            System.Text.RegularExpressions.CaptureCollection cs = System.Text.RegularExpressions.Regex.Match(str, @"([01]{8})+").Groups[1].Captures;
+            byte[] data = new byte[cs.Count];
+            for (int i = 0; i < cs.Count; i++)
+            {
+                data[i] = Convert.ToByte(cs[i].Value, 2);
+            }
+            byte[] datadec = MicrosoftDecompress(data);
+            return Encoding.Unicode.GetString(datadec, 0, datadec.Length);
+        }
+
+        /// <summary>
+        /// 兽语转二进制
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private string aniTobinary(string str)
+        {
+            string result = "";
+            string language = EncC.Text;
+            string tmp = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                tmp = str[i].ToString();
+
+                if (tmp.Equals(language[0].ToString()))
+                {
+                    result += dict[0];
+                }
+                else if (tmp.Equals(language[1].ToString()))
+                {
+                    result += dict[1];
+                }
+                else if (tmp.Equals(language[2].ToString()))
+                {
+                    result += dict[2];
+                }
+                else if (tmp.Equals(language[3].ToString()))
+                {
+                    result += dict[3];
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 二进制转兽语
+        /// </summary>
+        /// <param name="binary"></param>
+        /// <returns></returns>
+        private string binaryToAni(string binary)
+        {
+            string r = "";
+            string language = EncC.Text;
+            string tmp = "";
+
+            for (int i = 0; i < binary.Length; i += 2)
+            {
+                tmp = binary.Substring(i, 2);
+                if (tmp.Equals(dict[0]))
+                {
+                    r += language[0].ToString();
+                }
+                else if (tmp.Equals(dict[1]))
+                {
+                    r += language[1].ToString();
+                }
+                else if (tmp.Equals(dict[2]))
+                {
+                    r += language[2].ToString();
+                }
+                else if (tmp.Equals(dict[3]))
+                {
+                    r += language[3].ToString();
+                }
+            }
+
+            return r;
+        }
+        public string HComp(string intext)
+        {
+            string opt = "";
+            if(HCT.Checked)
+            {
+                opt = intext.Replace("magnet:?xt=urn:btih:", "$:M").Replace("114514", "$:1").Replace("来点涩涩","$:L").Replace("https://www.pixiv.net/artworks/","$:P");
+            }
+            else
+            {
+                opt = intext;
+            }
+            return opt;
+        }
+        private void Run_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Output.Clear();
+                string result = "";
+                string binary = "";
+
+                if (Mode.Checked)
+                {
+                    if (Input.Text != null)
+                    {
+                        if(EncC.Text.Contains("?")|| EncC.Text.Contains("&")|| EncC.Text.Contains("/"))
+                        {
+                            throw new Exception(@"编码字符不得包含下列字符：? & /" + "\r\nErrCode: -2");
+                        }
+                        if (Standard.Checked)
+                        {
+                            binary = StringToBinary(HComp(Input.Text));
+                            result = "roar://Raw?text=" + binaryToAni(binary) + "&encc=" + EncC.Text;
+                        }
+                        else
+                        {
+                            binary = StringToBinarywithComp(HComp(Input.Text));
+                            result = "roar://Deflate?text=" + binaryToAni(binary) + "&encc=" + EncC.Text;
+                        }
+
+                    }
+                    else
+                    {
+                        throw new Exception("输入内容不得为空\r\nErrCode: -1");
+                    }
+                }
+                else
+                {
+                    if (Input.Text != null)
+                    {
+                        //处理文本
+                        if(Input.Text.Contains("roar://") && Input.Text.Contains("&encc="))
+                        {
+                            string key = Input.Text.ToString().Substring(Input.Text.ToString().LastIndexOf(@"=") + 1);
+                            if(key.Length != 4)
+                            {
+                                throw new Exception("密文错误：编码字符不正确\r\nErrCode: 2");
+                            }
+                            EncC.Text = key;
+                            string[] tl = Input.Text.ToString().Split('?');
+                            string tp = tl[0].Substring(tl[0].LastIndexOf(@"/") + 1);
+                            if(tp.Contains("Raw"))
+                            {
+                                string[] rt = Input.Text.ToString().Split('&');
+                                string dt = rt[0].Replace("roar://Raw?text=","");
+                                binary = aniTobinary(dt);
+                                result = BinaryToString(binary);
+                            }
+                            else if(tp.Contains("Deflate"))
+                            {
+                                string[] rt = Input.Text.ToString().Split('&');
+                                string dt = rt[0].Replace("roar://Deflate?text=", "");
+                                binary = aniTobinary(dt);
+                                result = BinaryToStringwithComp(binary);
+                            }
+                            else
+                            {
+                                throw new Exception("密文错误：不支持的或未知的格式\r\nErrCode: 3");
+                            }
+                        }
+                        else
+                        {
+                            binary = aniTobinary(Input.Text);
+                            result = BinaryToString(binary);
+                        }
+                        
+                    }
+                    else
+                    {
+                        throw new Exception("输入内容不得为空\r\nErrCode: -1");
+                    }
+                }
+                Output.Text = result;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void HCT_CheckedChanged(object sender, EventArgs e)
+        {
+            if (HCT.Checked)
+            {
+                MessageBox.Show("警告：目前高级压缩只作为实验模式和压缩率对比，只提供“编码”功能，事后无法解码！");
+            }
+        }
+    }
+}
